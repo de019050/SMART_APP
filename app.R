029c5cd58fa1c0693bc8995d23e86603a0aee86e
de019050 <patric.stracke@sanofi.com>
2024-02-21 18:34:21 +0100

    commit

--- app.R
+++ app.R
@@ -11,26 +11,27 @@ library(stringr)
 #================================
 # Initial  data
 
-rds_filename_org <- "Data/alldat_2024-01-03.rds"
-alldat <- readRDS(rds_filename_org)
+# rds_filename_org <- "Data/alldat_2024-01-03.rds"
+# alldat <- readRDS(rds_filename_org)
 
 #================================
 # Single data
 
-ALL_REFLEX<-alldat%>%
-  filter(str_detect(alldat$status,"r_f")==TRUE)%>%
-  filter(!is.na(import_file))%>%
-  filter(!is.na(aql_aql))%>%
-  filter(!is.na(geraet))%>%    
-  dplyr::select(-c(import_file1,ref_auftr_nr,auftr_typ,storno_grund,storno_datum,four_eyes_check,preisli_nr,preisli_vers,kuli_bez,anz_zyklen,prob_nr,zykl_nr,param_art,param_bez,untergrenzen,obergrenzen))%>%
-  #filter(str_detect(alldat$status,"r_f")==TRUE)%>%
-  #filter(charge != "2F026")%>%
-  #filter(charge != "3F222")%>%
-  pivot_wider(id_cols=c(status,frei_datum,auftr_nr,mat_nr,charge,met_d_nr,mat_bez,anzahl_muster),
-              names_from = param_nr,
-              values_from = e_wert)%>%
-  mutate(order=row_number())
+singledata <- "Data/ALL_REFLEX.rds"
+ALL_REFLEX <- readRDS(singledata)
 
+# ALL_REFLEX<-alldat%>%
+#   filter(str_detect(alldat$status,"r_f")==TRUE)%>%
+#   filter(!is.na(import_file))%>%
+#   filter(!is.na(aql_aql))%>%
+#   filter(!is.na(geraet))%>%    
+#   dplyr::select(-c(import_file1,ref_auftr_nr,auftr_typ,storno_grund,storno_datum,four_eyes_check,preisli_nr,preisli_vers,kuli_bez,anz_zyklen,prob_nr,zykl_nr,param_art,param_bez,untergrenzen,obergrenzen))%>%
+#   pivot_wider(id_cols=c(status,frei_datum,auftr_nr,mat_nr,charge,met_d_nr,mat_bez,anzahl_muster,chargengroesse),
+#               names_from = param_nr,
+#               values_from = e_wert)%>%
+#   mutate(order=row_number())
+# 
+# write_rds(ALL_REFLEX,file="Data/ALL_REFLEX.rds")
 # 
 #================================
 
@@ -41,39 +42,44 @@ ALL_REFLEX<-alldat%>%
 #================================
 # Batch data
 
-Refelex_EDOs <- read_csv("Data/Reflex_EDO.csv")
-workdays <- read_csv("Data/workdays.csv")
-workdays<-workdays%>%
-  mutate(measurmenttype=MESSGROESSE)%>%
-  mutate(mean=MITTELWERT)%>%
-  mutate(sd=STANDARDABWEICHUNG)%>%
-  dplyr::select(measurmenttype,mean,sd)%>%
-  filter(measurmenttype!='PNEUMATIC')
+batchdata <- "Data/simstart_ALL_Reflex_Num.rds"
+simstart_ALL_Reflex_Num <- readRDS(batchdata)
 
-simstart_ALL_Reflex_Num<-ALL_REFLEX %>%
-  group_by(mat_bez,charge) %>% 
-  summarize(n=n(),mdose=mean(`R-P001`,na.rm=T), sdose=sd(`R-P001`,na.rm=T), mitime=mean(`R-P027`,na.rm=T),sitime=sd(`R-P027`,na.rm=T),midepth=mean(`R-P028`,na.rm=T),
-            sidepth=sd(`R-P028`,na.rm=T), 
-            mact=mean(`R-P026`,na.rm=T),sact=sd(`R-P026`,na.rm=T),mnpos=mean(`R-P029`,na.rm=T),snpos=sd(`R-P029`,na.rm=T))
-
-
-simstart_ALL_Reflex_Num<-simstart_ALL_Reflex_Num%>%mutate(newn = ifelse(n>=497,110,ifelse(n<=310 & n<=320,58,60)))%>%mutate(NewN20=round(newn*(1+0.2^2),digits=0))  
-
-simstart_ALL_Reflex_Num<-simstart_ALL_Reflex_Num%>%
-  #added constnts from ISO
-  mutate(kfac = ifelse(newn == 58, 2.592, ifelse(newn == 60, 2.5730, 2.449)))%>%
-  mutate(pfac = ifelse(newn == 58, 0.003872, ifelse(newn == 60, 0.004150, 0.006602)))%>%
-  mutate(fsfac = ifelse(newn == 58, 0.179, ifelse(newn == 60, 0.180, 0.180)))%>%
-  # added speclimits from EDOs
-  #first  one sided
-  mutate(dose_lsl= ifelse(mat_bez == "FITUSIRAN", 0.5, ifelse(mat_bez == "MARS", 1.14, 2.0)))%>%
-  mutate(itime_usl= ifelse(mat_bez == "FITUSIRAN", 15, ifelse(mat_bez == "MARS", 15, 15))) %>%
-  mutate(npos_usl= ifelse(mat_bez == "FITUSIRAN", 4.5, ifelse(mat_bez == "MARS", 4.5, 4.6)))%>%
-  # now 2-sided
-  mutate(act_lsl= ifelse(mat_bez == "FITUSIRAN", 3, ifelse(mat_bez == "MARS", 3, 3)))%>% 
-  mutate(act_usl= ifelse(mat_bez == "FITUSIRAN", 16, ifelse(mat_bez == "MARS", 16, 24)))%>%
-  mutate(idepth_lsl= ifelse(mat_bez == "FITUSIRAN", 4, ifelse(mat_bez == "MARS", 4, 4)))%>% 
-  mutate(idepth_usl= ifelse(mat_bez == "FITUSIRAN", 8, ifelse(mat_bez == "MARS", 8, 8)))
+# Refelex_EDOs <- read_csv("Data/Reflex_EDO.csv")
+# workdays <- read_csv("Data/workdays.csv")
+# workdays<-workdays%>%
+#   mutate(measurmenttype=MESSGROESSE)%>%
+#   mutate(mean=MITTELWERT)%>%
+#   mutate(sd=STANDARDABWEICHUNG)%>%
+#   dplyr::select(measurmenttype,mean,sd)%>%
+#   filter(measurmenttype!='PNEUMATIC')
+# 
+# simstart_ALL_Reflex_Num<-ALL_REFLEX %>%
+#   group_by(mat_bez,charge) %>% 
+#   summarize(n=n(),mdose=mean(`R-P001`,na.rm=T), sdose=sd(`R-P001`,na.rm=T), mitime=mean(`R-P027`,na.rm=T),sitime=sd(`R-P027`,na.rm=T),midepth=mean(`R-P028`,na.rm=T),
+#             sidepth=sd(`R-P028`,na.rm=T), 
+#             mact=mean(`R-P026`,na.rm=T),sact=sd(`R-P026`,na.rm=T),mnpos=mean(`R-P029`,na.rm=T),snpos=sd(`R-P029`,na.rm=T))
+# 
+# 
+# simstart_ALL_Reflex_Num<-simstart_ALL_Reflex_Num%>%mutate(newn = ifelse(n>=497,110,ifelse(n<=310 & n<=320,58,60)))%>%mutate(NewN20=round(newn*(1+0.2^2),digits=0))  
+# 
+# simstart_ALL_Reflex_Num<-simstart_ALL_Reflex_Num%>%
+#   #added constnts from ISO
+#   mutate(kfac = ifelse(newn == 58, 2.592, ifelse(newn == 60, 2.5730, 2.449)))%>%
+#   mutate(pfac = ifelse(newn == 58, 0.003872, ifelse(newn == 60, 0.004150, 0.006602)))%>%
+#   mutate(fsfac = ifelse(newn == 58, 0.179, ifelse(newn == 60, 0.180, 0.180)))%>%
+#   # added speclimits from EDOs
+#   #first  one sided
+#   mutate(dose_lsl= ifelse(mat_bez == "FITUSIRAN", 0.5, ifelse(mat_bez == "MARS", 1.14, 2.0)))%>%
+#   mutate(itime_usl= ifelse(mat_bez == "FITUSIRAN", 15, ifelse(mat_bez == "MARS", 15, 15))) %>%
+#   mutate(npos_usl= ifelse(mat_bez == "FITUSIRAN", 4.5, ifelse(mat_bez == "MARS", 4.5, 4.6)))%>%
+#   # now 2-sided
+#   mutate(act_lsl= ifelse(mat_bez == "FITUSIRAN", 3, ifelse(mat_bez == "MARS", 3, 3)))%>% 
+#   mutate(act_usl= ifelse(mat_bez == "FITUSIRAN", 16, ifelse(mat_bez == "MARS", 16, 24)))%>%
+#   mutate(idepth_lsl= ifelse(mat_bez == "FITUSIRAN", 4, ifelse(mat_bez == "MARS", 4, 4)))%>% 
+#   mutate(idepth_usl= ifelse(mat_bez == "FITUSIRAN", 8, ifelse(mat_bez == "MARS", 8, 8)))
+# 
+# write_rds(simstart_ALL_Reflex_Num,file="Data/simstart_ALL_Reflex_Num.rds")
 
 #================================
 
